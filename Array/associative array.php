c0386c1418c8467a431d236e9ef06db536776301
Riyakhushalani <154345324+Riyakhushalani@users.noreply.github.com>
2024-02-22 19:46:31 +0530

    Add files via upload

---
+++ Array/associative array.php
@@ -0,0 +1,4 @@
+<?php
+$student_detail=array("name"=>"Riya","rollno"=>14,"address"=>"Rajkot","department"=>"cs&it","university"=>"Atmiya","class"=>"B2","dob"=>"17-09-2004");
+print_r($student_detail);
+?>
---
+++ Array/multidimensional array.php
@@ -0,0 +1,10 @@
+
+$student_master_main=array(array("name"=>"riya","rollno"=>14,"address"=>"Rajkot","department"=>"cs&it",
+"university"=>"Atmiya","class"=>"B2",
+"dob"=>"17-09-2004"),array("name"=>"riya","rollno"=>14,"address"=>"Rajkot","department"=>"cs&it",
+"university"=>"Atmiya","class"=>"B2",
+"dob"=>"17-09-2004"));
+
+
+print_r($student_master_main);
+?>
